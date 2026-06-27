c828c3264a131081ac59404f4961577df0e9d748
Janelle Santos <2023100415@ms.bulsu.edu.ph>
2026-06-27 19:39:39 +0800

    updated reports

--- capstone-project/src/data/barangays.js
+++ capstone-project/src/data/barangays.js
@@ -1,4 +1,5 @@
 export const barangays = [
+  "All",
   "Anilao",
   "Atlag",
   "Babatnin",
--- capstone-project/src/data/familyPlanningMethods.js
+++ capstone-project/src/data/familyPlanningMethods.js
@@ -1,14 +1,15 @@
 export const familyPlanningMethods = [
-  "Condom (Male)",
-  "Condom (Female)",
-  "Birth Control Pills",
-  "Injectables (Depo-Provera)",
-  "Implant (Subdermal Implant)",
-  "Intrauterine Device (IUD)",
-  "Tubal Ligation (Female Sterilization)",
-  "Vasectomy (Male Sterilization)",
-  "Calendar/Rhythm Method",
-  "Withdrawal Method",
-  "Lactational Amenorrhea Method (LAM)",
-  "Fertility Awareness Method"
+  "All",
+  "Condom",
+  "IUD",
+  "Pills",
+  "Injectable",
+  "Vasectomy",
+  "Tubal Ligation",
+  "Implant",
+  "Cervical Mucus Method (CMM)",
+  "Basal Body Temperature (BBT)",
+  "Sympto-Thermal Method (STM)",
+  "Standard Days Method (SDM)",
+  "Lactational Amenorrhea Method (LAM)"
 ];
